ca372b40f351fffa8160f41da6bb0911e65a618a
Lana <al.t.loureiro0@gmail.com>
2026-05-05 09:00:26 -0300

    Add font dependencies and create theme file for styling

---
+++ style/theme.js
@@ -0,0 +1,14 @@
+export const COLORS = {
+  primary: '#00142E',
+  secondary: '#0562D2',
+  darkNeutral: '#000000',
+  lightNeutral: '#FFFFFF',
+  darkGrey: '#333333',
+  lightGrey: '#666666',
+};
+
+export const FONT = {
+  title: 'Anton_400Regular',
+  body: 'TitilliumWeb_400Regular',
+  bodyBold: 'TitilliumWeb_700Bold'
+};
